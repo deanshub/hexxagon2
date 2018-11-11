72c8773bc3d563dcc3d9e1e3332ab4dc7da86efd
Dean Shub <deanshub@gmail.com>
2018-11-11 11:39:07 +0200

    adding player pieces

--- src/Cell/Cell.js
+++ src/Cell/Cell.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import classnames from 'classnames'
-import style from './cell.module.css'
 import { observer, inject } from 'mobx-react'
 import { compose } from 'recompose'
+import style from './cell.module.css'
+import Piece from '../Piece'
 
 class Cell extends Component {
   render() {
@@ -12,12 +13,15 @@ class Cell extends Component {
         onClick={()=>console.log(position, boardStore.getPossibleMoves(position))}
         className={classnames(
           style.cell,
-          {[style.hidden]: value===undefined}
+          {[style.hidden]: value===undefined},
+          {[style.active]: value!==0},
         )}
       >
         <div className={classnames(style.content)}>
           <div className={classnames(style.left)} />
-          <div className={classnames(style.middle)}>{value||''}</div>
+          <div className={classnames(style.middle)}>
+            {value!==0&&<Piece value={value}/>}
+          </div>
           <div className={classnames(style.right)} />
         </div>
       </div>
---
+++ src/Piece/Piece.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import classnames from 'classnames'
+import style from './piece.module.css'
+
+export default function ({value}){
+  return (
+    <div className={classnames(style.tridiv)}>
+      <div className={classnames(style.scene)}>
+        <div
+          className={classnames(
+            style.shape,
+            style.cylinder,
+            style.cyl,
+            {[style.p1]:value===1},
+            {[style.p2]:value===2},
+          )}
+        >
+          <div className={classnames(style.face, style.bm)}>
+            <div className={classnames(style.photonShader)}></div>
+          </div>
+          <div className={classnames(style.face, style.tp)}>
+            <div className={classnames(style.photonShader)}></div>
+          </div>
+          <div className={classnames(style.face, style.side, style.s0)}>
+            <div className={classnames(style.photonShader)}></div>
+          </div>
+          <div className={classnames(style.face, style.side, style.s1)}>
+            <div className={classnames(style.photonShader)}></div>
+          </div>
+          <div className={classnames(style.face, style.side, style.s2)}>
+            <div className={classnames(style.photonShader)}></div>
+          </div>
+          <div className={classnames(style.face, style.side, style.s3)}>
+            <div className={classnames(style.photonShader)}></div>
+          </div>
+          <div className={classnames(style.face, style.side, style.s4)}>
+            <div className={classnames(style.photonShader)}></div>
+          </div>
+          <div className={classnames(style.face, style.side, style.s5)}>
+            <div className={classnames(style.photonShader)}></div>
+          </div>
+          <div className={classnames(style.face, style.side, style.s6)}>
+            <div className={classnames(style.photonShader)}></div>
+          </div>
+          <div className={classnames(style.face, style.side, style.s7)}>
+            <div className={classnames(style.photonShader)}></div>
+          </div>
+          <div className={classnames(style.face, style.side, style.s8)}>
+            <div className={classnames(style.photonShader)}></div>
+          </div>
+          <div className={classnames(style.face, style.side, style.s9)}>
+            <div className={classnames(style.photonShader)}></div>
+          </div>
+          <div className={classnames(style.face, style.side, style.s10)}>
+            <div className={classnames(style.photonShader)}></div>
+          </div>
+          <div className={classnames(style.face, style.side, style.s11)}>
+            <div className={classnames(style.photonShader)}></div>
+          </div>
+          <div className={classnames(style.face, style.side, style.s12)}>
+            <div className={classnames(style.photonShader)}></div>
+          </div>
+          <div className={classnames(style.face, style.side, style.s13)}>
+            <div className={classnames(style.photonShader)}></div>
+          </div>
+          <div className={classnames(style.face, style.side, style.s14)}>
+            <div className={classnames(style.photonShader)}></div>
+          </div>
+          <div className={classnames(style.face, style.side, style.s15)}>
+            <div className={classnames(style.photonShader)}></div>
+          </div>
+          <div className={classnames(style.face, style.side, style.s16)}>
+            <div className={classnames(style.photonShader)}></div>
+          </div>
+          <div className={classnames(style.face, style.side, style.s17)}>
+            <div className={classnames(style.photonShader)}></div>
+          </div>
+          <div className={classnames(style.face, style.side, style.s18)}>
+            <div className={classnames(style.photonShader)}></div>
+          </div>
+          <div className={classnames(style.face, style.side, style.s19)}>
+            <div className={classnames(style.photonShader)}></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
