15840b086ed6aef15d37004255ba9c7652ef2fae
Dean Shub <deanshub@gmail.com>
2019-06-07 19:50:04 +0300

    new score board

--- src/Scores/Scores.js
+++ src/Scores/Scores.js
@@ -3,28 +3,31 @@ import { observer, inject } from 'mobx-react'
 import { compose } from 'recompose'
 import classnames from 'classnames'
 import style from './scores.module.css'
+import Piece from '../Piece'
 
 class Scores extends Component {
   renderScore(player, score) {
+    const { boardStore } = this.props
     return (
-      <div key={player}>
-        Player{player}: {score}
+      <div
+        className={classnames(style.playerScore, {
+          [style.current]: boardStore.currentPlayer === player,
+        })}
+        key={player}
+      >
+        <Piece value={player} />
+        <div>{score}</div>
       </div>
     )
   }
   render() {
     const { boardStore } = this.props
     return (
-      <>
-        <div className={classnames(style.scores)}>
-          {Object.keys(boardStore.scores).map(player =>
-            this.renderScore(player, boardStore.scores[player])
-          )}
-        </div>
-        <div className={classnames(style.turn)}>
-          Turn: Player{boardStore.currentPlayer}
-        </div>
-      </>
+      <div className={classnames(style.scores)}>
+        {Object.keys(boardStore.scores).map(player =>
+          this.renderScore(Number(player), boardStore.scores[player])
+        )}
+      </div>
     )
   }
 }
